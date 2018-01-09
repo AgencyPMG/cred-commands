6a6f75f1b3430db9180853fadff1608e008ca6d6
Christopher Davis <cdavis9999@gmail.com>
2018-01-09 12:47:03 -0600

    Introduce & Test the PutCommand

---
+++ src/Command/PutCommand.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of pmg/cred-commands
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\CredCommands\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Put a single parameter from the parameter store.
+ *
+ * @since 1.0
+ */
+final class PutCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('creds:put');
+        $this->setDescription('Put a credential into the parameter store');
+        $this->addArgument(
+            'credential',
+            InputArgument::REQUIRED,
+            'The credential to put'
+        );
+        $this->addArgument(
+            'value',
+            InputArgument::REQUIRED,
+            'The credential value'
+        );
+        $this->setHelp(<<<END
+The <info>%command.name%</info> command puts a new credential value into the parameter
+store.
+
+    %command.full_name% some_cred aValue
+
+If the value contains dashes or other character that may be construed as options,
+use the double dash (end of arguments) to avoid parsing the value.
+
+    %command.full_name% some_cred -- -aValue-with-dashes
+END
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $in, OutputInterface $out)
+    {
+        $ver = $this->client->put(
+            $in->getArgument('credential'),
+            $in->getArgument('value')
+        );
+
+        $out->writeln(sprintf(
+            'put %s: version %d',
+            $in->getArgument('credential'),
+            $ver
+        ));
+    }
+}
---
+++ test/Command/PutCommandTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of pmg/cred-commands
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\CredCommands\Command;
+
+class PutCommandTest extends CommandTestCase
+{
+    public function testCommandAddsANewValueToTheParamterStore()
+    {
+        $this->client->expects($this->once())
+            ->method('put')
+            ->with(self::CREDENTIAL, 'testing123')
+            ->willReturn(1);
+
+        [$tester, $status] = $this->executeCommand([
+            'credential' => self::CREDENTIAL,
+            'value' => 'testing123',
+        ]);
+
+        $this->assertSame(0, $status);
+        $this->assertContains(
+            sprintf('put %s: version 1', self::CREDENTIAL),
+            $tester->getDisplay()
+        );
+    }
+
+    protected function createCommand() : Command
+    {
+        return new PutCommand($this->client);
+    }
+}
